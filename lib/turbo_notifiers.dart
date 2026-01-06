31af896ba6fe8a12b08fc3f64a97aeb85c3b1171
appboypov <info@codaveto.com>
2026-01-06 19:37:05 +0100

    Release v1.0.0: Rename Informer to TurboNotifier, remove specialized informers

---
+++ lib/turbo_notifiers.dart
@@ -0,0 +1,4 @@
+library turbo_notifiers;
+
+export 'turbo_notifier.dart';
+export 'turbo_change_notifier.dart';
---
+++ test/notifiers_test.dart
@@ -0,0 +1,5 @@
+// Tests will be added in future versions
+
+void main() {
+  // TODO: Add TurboNotifier tests
+}
